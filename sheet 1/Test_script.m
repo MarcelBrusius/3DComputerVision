fb34d4cf82cc1a94d2973cd4a79dcfc1e1407133
Marcel <brusius@rhrk.uni-kl.de>
2016-11-17 15:18:49 +0100

    - Replaced the use of "inv", and introduced the backslash operator: more robust - Fixed an issue computing r (forgot the colon for the second dimention)

--- sheet 1/Test_script.m
+++ sheet 1/Test_script.m
@@ -1,15 +1,17 @@
 image = imread('00001.jpg');
 load('Calib_Results.mat');
 
-Plane = projectPoints(X_2, KK, Rc_2, Tc_2, 'Dist',kc);
+Plane_Dist = projectPoints(X_2, KK, Rc_2, Tc_2, 'Dist',kc);
+Plane = projectPoints(X_2, KK, Rc_2, Tc_2);
 
 H = figure;
 imagesc([-320 320], [-240 240],image);
 hold on;
 % H = figure;
-plot(Plane(:,1),Plane(:,2),'r*');
+plot(Plane_Dist(:,1),Plane_Dist(:,2),'r*');
 % figure;
-% plot(X_8(1,:),-X_8(2,:),'bx');
+hold on;
+plot(Plane(:,1),Plane(:,2),'bx');
 % G = H.CurrentAxes;
 % G.YLim = [-240 240];
 % G.XLim = [-320 320];
--- sheet 1/projectPoints.m
+++ sheet 1/projectPoints.m
@@ -12,19 +12,16 @@ function [ Plane ] = projectPoints ( World , Intrinsic , Rotation, Translation, 
         disp('Radial distortion considered.');
     end
     
-    inverse_Intrinsic = inv(Intrinsic);
+    %inverse_Intrinsic = inv(Intrinsic);
     
     Plane = Intrinsic*[Rotation Translation]*[World;ones(1,40)];
     
-    
-    
-    normalized_Plane = inverse_Intrinsic * Plane;
-    r = norm(normalized_Plane(1:2),2);
-    dist_free_Plane = normalized_Plane * (1+var.Dist(1)*r^2 + var.Dist(2)*r^4 + var.Dist(5)*r^6) ;
+    normalized_Plane = Intrinsic\Plane; % more robust calculation than using the "inv" function
+    % Fixed an issue computing r, forgot the colon for the second dimention
+    r = norm(normalized_Plane(1:2,:),2);
+    dist_free_Plane = normalized_Plane * (1+var.Dist(1)*r^2 + var.Dist(2)*r^4 + var.Dist(5)*r^6);
     
     Plane = dist_free_Plane' * Intrinsic;
-
-
 end
 
 
