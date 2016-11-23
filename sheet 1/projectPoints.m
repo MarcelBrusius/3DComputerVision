fe236b2a55850e4adb1a27e1c94fb06db7e4be16
Leif Eric Goebel <lgoebel@rhrk.uni-kl.de>
2016-11-23 10:11:17 +0100

    Übungsblatt getext. Die zweite Matlab Funktion kommt gleich noch. Müssen ja nur das Testskript ändern.

--- sheet 1/projectPoints.m
+++ sheet 1/projectPoints.m
@@ -17,12 +17,12 @@ function [ Plane ] = projectPoints ( World , Intrinsic , Rotation, Translation, 
     
     Plane = Intrinsic*[Rotation Translation]*[World;ones(1,n)]; 
     
-    Plane = Plane./Plane(3,:); % 3rd entry should be one (wasn't), now correctly scaled to [- 320 320] x [-240 240]
+    Plane = Plane./repmat(Plane(3,:),[3,1]); % 3rd entry should be one (wasn't), now correctly scaled to [- 320 320] x [-240 240]
     
     normalized_Plane = Intrinsic\Plane; % more robust calculation than using the "inv" function
     dist_free_Plane = ones(3,n);
     for i = 1:n % iteration to correctly calculate the norm "r" for each world point
-    % Fixed an issue computing r, forgot the colon for the second dimention
+    % Fixed an issue computing r, forgot the colon for the second dimension
     r = norm(normalized_Plane(1:2,i),2);
     dist_free_Plane(1:2,i) = normalized_Plane(1:2,i) * (1+var.Dist(1)*r^2 + var.Dist(2)*r^4 + var.Dist(5)*r^6);
     end
